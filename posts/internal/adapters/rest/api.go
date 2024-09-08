470d96ddf1530bef90179a0801081304a1d4649b
Mustafa Ozyurt <mustafa.ozyurt@gmail.com>
2024-09-08 17:41:07 +0300

    get post

--- posts/internal/adapters/rest/api.go
+++ posts/internal/adapters/rest/api.go
@@ -8,6 +8,7 @@ import (
 	"github.com/fmo/hexagonal-blog/internal/ports"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type Adapter struct {
@@ -27,23 +28,48 @@ type Response struct {
 func (a Adapter) Run(ctx context.Context) {
 
 	http.HandleFunc("/posts", func(w http.ResponseWriter, r *http.Request) {
-		_, err := a.api.SavePost(ctx, domain.NewPost("title", "body"))
+		if r.Method != http.MethodPost {
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+			return
+		}
+
+		var post domain.Post
+		err := json.NewDecoder(r.Body).Decode(&post)
+		if err != nil {
+			http.Error(w, "Bad request", http.StatusBadRequest)
+			return
+		}
+
+		savedPost, err := a.api.SavePost(ctx, post)
 		if err != nil {
 			log.Fatalf("cant save")
-		}
-	})
-
-	http.HandleFunc("/posts/{postId}", func(w http.ResponseWriter, r *http.Request) {
-		post, _ := a.api.GetPost(ctx, 1)
-
-		response := Response{
-			Message: post,
-			Status:  200,
 		}
 
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(response)
+		json.NewEncoder(w).Encode(Response{
+			Message: savedPost,
+			Status:  200,
+		})
+	})
+
+	http.HandleFunc("/post", func(w http.ResponseWriter, r *http.Request) {
+		query := r.URL.Query()
+		postIdStr := query.Get("postId")
+		postId, err := strconv.Atoi(postIdStr)
+		if err != nil {
+			http.Error(w, "Invalid post ID", http.StatusBadRequest)
+			return
+		}
+
+		post, _ := a.api.GetPost(ctx, int64(postId))
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(Response{
+			Message: post,
+			Status:  200,
+		})
 	})
 
 	err := http.ListenAndServe(fmt.Sprintf(":%d", a.port), nil)
