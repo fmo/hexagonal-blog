390be437749f2394e540ba27448170e3c59908c2
Mustafa Ozyurt <mustafa.ozyurt@gmail.com>
2024-09-08 10:56:39 +0300

    main impl

--- posts/cmd/main.go
+++ posts/cmd/main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"github.com/fmo/hexagonal-blog/config"
+	"github.com/fmo/hexagonal-blog/internal/adapters/db/mysql"
+	"github.com/fmo/hexagonal-blog/internal/adapters/rest"
+	"github.com/fmo/hexagonal-blog/internal/application/core/api"
+	"github.com/joho/godotenv"
 	log "github.com/sirupsen/logrus"
 	"os"
 )
@@ -11,5 +16,20 @@ func init() {
 }
 
 func main() {
+	environment := os.Getenv("ENVIRONMENT")
+	if environment != "production" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatalf("Error loading .env file")
+		}
+	}
 
+	dbAdapter, err := mysql.NewAdapter(config.GetDataSourceURL())
+	if err != nil {
+		log.Fatalf("Failed to connect to database. Error: %v", err)
+	}
+
+	application := api.NewApplication(dbAdapter)
+	restAdapter := rest.NewAdapter(application, config.GetApplicationPort())
+	restAdapter.Run()
 }
